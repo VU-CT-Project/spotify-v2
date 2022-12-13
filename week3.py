6610cfadc5265a3eee2ca5b4683977c88ea0d1a3
Shroominic <db2002dominic@gmail.com>
2022-12-13 18:04:10 +0100

    init week3

---
+++ week3.py
@@ -0,0 +1,10 @@
+""" Week 3 - Shifts
+In the third week after the launch, you want to account for a user's mood shift. 
+We have a few types of songs: “happy”, “party”, “calming”, and “lounge.” 
+Define in your own algorithm if you think one song can be classified as two of those at the same time.  
+ 
+This week you need to select 5 more songs to suggest to each user, again. 
+If last week the user was playing a lot of songs from one of those types, this week we want to suggest more of the same type. 
+Define, in your own terms what this should mean: if the user listened to 3 “calming” songs, 
+should we provide 3 more without considering other factors? What if he or she also listened to 3 “party” songs and 4 “lounge” style songs?
+"""
